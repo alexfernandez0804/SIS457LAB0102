fcea206e553ba5a3b698156b14df193472667af7
alexfernandez0804 <alexpaulfernandez0804@gmail.com>
2022-04-01 20:18:41 -0400

    primer commit

---
+++ principal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+	int respuesta;
+	cout << "Mi nombre es Alexander Paul Fernandez Acho, nacido el 8 de abril del 2003, segundo hijo de los señores " << endl;
+	cout << "Jaime Fernandez Cervantes y Ruth Bethzabeth Acho Sanchez. Graduado del colegio San Juanillo con honores el año 2020 " << endl;
+	cout << "con conociminetos de radioelectronica y electricidad. Egresado del Centro Boliviano Americano (CBA) con conocimientos " << endl;
+	cout << "de ingles avanzado. Vivo en la ciudad de Sucre-Bolivia desde que naci, aunque siempre quise vivir en Santa Cruz." << endl;
+	cout << "Me gusta el Voleyball y los videojuegos, me gusta ser competitivo en esas cosas." << endl;
+	cout << "Ingrese a la Carrera de Ingenieria de Sistemas porque me llama mucho la atencion, tomando en cuenta de que" << endl;
+	cout << "es de las carreras con mas demanda en los ultimos años." << endl;
+	cout << "\n Desea volver a ver la Biografia? Presione (1) si lo desea - Presione (2) para salir" << endl;
+	cout << "\n";
+	cin >> respuesta;
+
+	if (respuesta == 1) {
+		cout << "Mi nombre es Alexander Paul Fernandez Acho, nacido el 8 de abril del 2003, segundo hijo de los señores " << endl;
+		cout << "Jaime Fernandez Cervantes y Ruth Bethzabeth Acho Sanchez. Graduado del colegio San Juanillo con honores el año 2020 " << endl;
+		cout << "con conociminetos de radioelectronica y electricidad. Egresado del Centro Boliviano Americano (CBA) con conocimientos " << endl;
+		cout << "de ingles avanzado. Vivo en la ciudad de Sucre-Bolivia desde que naci, aunque siempre quise vivir en Santa Cruz." << endl;
+		cout << "Me gusta el Voleyball y los videojuegos, me gusta ser competitivo en esas cosas." << endl;
+		cout << "Ingrese a la Carrera de Ingenieria de Sistemas porque me llama mucho la atencion, tomando en cuenta de que" << endl;
+		cout << "es de las carreras con mas demanda en los ultimos años." << endl;
+	}
+	return 0;
+}
